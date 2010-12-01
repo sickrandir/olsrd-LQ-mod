883ff7077262fd69c6dcb11f1cd2a167e8d97b80
kinto <kinto@kinto-desktop.(none)>
2010-12-01 12:33:56 +0100

    no binaries

--- src/builddata.c
+++
@@ -1,4 +0,0 @@
-#include "defs.h"
-const char olsrd_version[] = "olsr.org -  0.6.1-git_-hash_13853dbbe419f055a7f58f8a967797e7  -";
-const char build_date[] = "2010-11-30 10:29:29";
-const char build_host[] = "kinto-laptop";
