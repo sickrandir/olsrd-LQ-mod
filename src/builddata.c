bbe7524ce3599542476842018c0f1ff2c45621a4
kinto <kinto@kinto-desktop.(none)>
2011-01-05 18:31:14 +0100

    lq_plugin_default_hybrid_PLC gets plc update functionality. It compiles.

---
+++ src/builddata.c
@@ -0,0 +1,4 @@
+#include "defs.h"
+const char olsrd_version[] = "olsr.org -  0.6.1-git_59af74b-hash_a44a0162f046599bcbbb21bcfde9ea64  -";
+const char build_date[] = "2011-01-05 18:12:45";
+const char build_host[] = "kinto-desktop";
